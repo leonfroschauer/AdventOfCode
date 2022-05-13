0a49f7e19de1445a848f556399c54b38150e3a38
Leon Froschauer <89684749+leonfroschauer@users.noreply.github.com>
2022-05-13 15:11:22 +0200

    Added solution template (python)

---
+++ aoc/templates/solution_template.py
@@ -0,0 +1,17 @@
+with open("input.txt") as file:
+    puzzle = file.read().strip()
+    plines = puzzle.splitlines()
+
+
+def part_1():
+    pass
+
+
+print(part_1())
+
+
+def part_2():
+    pass
+
+
+print(part_2())
